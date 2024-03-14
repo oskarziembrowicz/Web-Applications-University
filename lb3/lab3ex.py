4fcd538a24c6bbf0035e8049b36cda3747855003
oskarziembrowicz <rakson2002@gmail.com>
2024-03-14 13:53:19 +0100

    Reading data in ex15

--- lb3/lab3ex.py
+++ lb3/lab3ex.py
@@ -68,3 +68,28 @@ import socket
 # =======================================================================
 
 # Zad15
+
+datagram = "45 00 00 4e f7 fa 40 00 38 06 9d 33 d4 b6 18 1b c0 a8 00 02 0b 54 b9 a6 fb f9 3c 57 c1 0a 06 c1 80 18 00 e3 ce 9c 00 00 01 01 08 0a 03 a6 eb 01 00 0b f8 e5 6e 65 74 77 6f 72 6b 20 70 72 6f 67 72 61 6d 6d 69 6e 67 20 69 73 20 66 75 6e"
+datagram = datagram.replace(' ', '')
+
+protocol_ver_hex = datagram[0]
+SRC_IP_START = int(96/4)
+SRC_IP_END = int(128/4)
+src_ip_hex = datagram[SRC_IP_START:SRC_IP_END]
+DST_IP_START = int(128/4)
+DST_IP_END = int(160/4)
+dst_ip_hex = datagram[DST_IP_START:DST_IP_END]
+PROTOCOL_START = int(72/4)
+PROTOCOL_END = int(80/4)
+protocol_hex = datagram[PROTOCOL_START:PROTOCOL_END]
+
+SRC_PORT_START = int(160/4)
+SRC_PORT_END = int(176/4)
+src_port_hex = datagram[SRC_PORT_START:SRC_PORT_END]
+DST_PORT_START = int(176/4)
+DST_PORT_END = int(192/4)
+dst_port_hex = datagram[DST_PORT_START:DST_PORT_END]
+DATA_START = int(320/4)
+data_hex = datagram[DATA_START:]
+
+print(protocol_hex)
