d3db6f8ef85d5e1b251c603242731b094e6c709c
oskarziembrowicz <rakson2002@gmail.com>
2024-03-21 12:37:04 +0100

    Finishing ex4

--- lb4/lab4ex1client.py
+++ lb4/lab4ex1client.py
@@ -1,14 +1,56 @@
 import socket
 
-# Zad 1,2
+# Zad 1,2,3
+
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket.settimeout(5)
+#
+# try:
+#     client_socket.connect(('127.0.0.1', 2900))
+#     client_socket.send('Hello'.encode())
+#     data = client_socket.recv(4096)
+#     if data:
+#         print(f"Recieved: {data.decode()}")
+#     else:
+#         print("No data recieved :(")
+# except socket.error as e:
+#     print(f"Socket error: {e}")
+#
+# client_socket.close()
+
+# ============================================
+
+# Zad4
+
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket.settimeout(5)
+#
+# try:
+#     client_socket.connect(('127.0.0.1', 2900))
+#     client_socket.send('2+6'.encode())
+#     data = client_socket.recv(1024)
+#     if data:
+#         print(f"Recieved: {data.decode()}")
+#     else:
+#         print("No data recieved :(")
+# except socket.error as e:
+#     print(f"Socket error: {e}")
+#
+# client_socket.close()
+
+#=============================================================
+
+# Zad5
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.settimeout(5)
 
 try:
     client_socket.connect(('127.0.0.1', 2900))
-    client_socket.send('Hello'.encode())
-    data = client_socket.recv(4096)
+    client_socket.send('2+6'.encode())
+    # client_socket.send('+'.encode())
+    # client_socket.send('6'.encode())
+    data = client_socket.recv(1024)
     if data:
         print(f"Recieved: {data.decode()}")
     else:
---
+++ lb4/lab4ex4server.py
@@ -0,0 +1,30 @@
+import socket
+import datetime
+HOST = '127.0.0.1'
+PORT = 2900
+
+connected_clients_sockets = []
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+server_socket.listen(10)
+
+print("Waiting for clients...")
+
+connected_clients_sockets.append(server_socket)
+
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    print("Client connected")
+    try:
+        data = client_socket.recv(1024)
+        output = eval(data)
+        print(f"Recieved a message: {data.decode()}")
+        client_socket.send(f'{output}'.encode())
+    except:
+        client_socket.close()
+
+server_socket.close()
