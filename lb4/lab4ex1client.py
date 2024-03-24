9ddff9015d858e507e251b6bb282f54e1250b5b8
oskarziembrowicz <rakson2002@gmail.com>
2024-03-24 14:19:40 +0100

    Excercise 9 completed

--- lb4/lab4ex1client.py
+++ lb4/lab4ex1client.py
@@ -82,21 +82,56 @@ import socket
 
 # Zad7
 
-MAX_LENGTH = 20
-def fitTheMessage(message):
-    if len(message) < MAX_LENGTH:
-        while len(message) < MAX_LENGTH:
-            message += ' '
-    elif len(message) > MAX_LENGTH:
-        message = message[slice(0, MAX_LENGTH)]
-    return message
+# MAX_LENGTH = 20
+# def fitTheMessage(message):
+#     if len(message) < MAX_LENGTH:
+#         while len(message) < MAX_LENGTH:
+#             message += ' '
+#     elif len(message) > MAX_LENGTH:
+#         message = message[slice(0, MAX_LENGTH)]
+#     return message
+#
+# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# client_socket.settimeout(5)
+#
+# try:
+#     client_socket.connect(('127.0.0.1', 2901))
+#     client_socket.send(fitTheMessage('Hello').encode())
+#     data = client_socket.recv(1024)
+#     if data:
+#         print(f"Recieved: {data.decode()}")
+#     else:
+#         print("No data recieved :(")
+# except socket.error as e:
+#     print(f"Socket error: {e}")
+#
+# client_socket.close()
+
+#======================================================================
+
+# Zad9
+
+datagram = "ed740b550024effd70726f6772616d6d696e6720696e20707974686f6e2069732066756e"
+
+src_port_hex = datagram[0:4]
+dst_port_hex = datagram[4:8]
+data_hex = datagram[16:]
+
+src_port = int(src_port_hex, 16)
+dst_port = int(dst_port_hex, 16)
+data_length = int((len(data_hex)*4)/8)
+data = bytes.fromhex(data_hex).decode('ascii')
+
+output = f"zad14odp;src;{src_port};dst;{dst_port};data;{data}"
+
+print(output)
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.settimeout(5)
 
 try:
     client_socket.connect(('127.0.0.1', 2901))
-    client_socket.send(fitTheMessage('Hello').encode())
+    client_socket.send(output.encode())
     data = client_socket.recv(1024)
     if data:
         print(f"Recieved: {data.decode()}")
---
+++ lb4/lab4ex9server.py
@@ -0,0 +1,51 @@
+import socket
+import datetime
+HOST = '127.0.0.1'
+PORT = 2901
+
+def check_msg_syntax(txt):
+    s = len(txt.split(";"))
+    if s != 7:
+        return "BAD_SYNTAX"
+    else:
+        tmp = txt.split(";")
+        if tmp[0] == "zad14odp" and tmp[1] == "src" and tmp[3] == "dst" and tmp[5] == "data":
+            try :
+                src_port = int(tmp[2])
+                dst_port = int(tmp[4])
+                data = tmp[6]
+            except :
+                return "BAD_SYNTAX"
+            if src_port == 60788 and dst_port == 2901 and data == "programming in python is fun":
+                return "TAK"
+            else:
+                return "NIE"
+        else:
+            return "BAD_SYNTAX"
+
+connected_clients_sockets = []
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+server_socket.listen(10)
+
+print("Waiting for clients...")
+
+connected_clients_sockets.append(server_socket)
+
+
+while True:
+    client_socket, client_address = server_socket.accept()
+    print("Client connected")
+    try:
+        data = client_socket.recv(1024)
+        print(f"Recieved a message: {data.decode()}")
+        output = check_msg_syntax(data.decode())
+        print(output)
+        client_socket.send(output.encode())
+    except:
+        client_socket.close()
+
+server_socket.close()
