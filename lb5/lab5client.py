b6e1a2cf6987b1132974e3e9d2f86a4ace98ebb4
oskarziembrowicz <rakson2002@gmail.com>
2024-04-04 14:17:17 +0200

    Finishing drafts for all except ex 7

--- lb5/lab5client.py
+++ lb5/lab5client.py
@@ -25,36 +25,54 @@ import requests
 #===============================================
 
 # Zad3
+# NO SERVER
+# DO NOT DO?
 
-name = input("Enter your name: ")
-phone = input("Enter youe phone number: ")
-email = input("Enter your email address: ")
-size = input("Do you want your pizza small, medium or large?")
+#===============================================
 
-# Make sure this is an array:
-num_of_tops = input("How many topping do you want from: bacon, cheese, mushroom, onion?")
-print("Enter yout toppings")
-toppings = list()
-for i in range(int(num_of_tops)):
-    topp = input()
-    toppings.append(topp)
+# Zad4
 
-time = input("At what time do you want your delivery?")
-comments = input("Enter comments to your order or skip: ")
+# name = input("Enter your name: ")
+# phone = input("Enter youe phone number: ")
+# email = input("Enter your email address: ")
+# size = input("Do you want your pizza small, medium or large?")
+#
+# # Make sure this is an array:
+# num_of_tops = input("How many topping do you want from: bacon, cheese, mushroom, onion?")
+# print("Enter yout toppings")
+# toppings = list()
+# for i in range(int(num_of_tops)):
+#     topp = input()
+#     toppings.append(topp)
+#
+# time = input("At what time do you want your delivery?")
+# comments = input("Enter comments to your order or skip: ")
+#
+# data = {
+#     "form": {
+#         "comments": comments,
+#         "custemail": email,
+#         "custname": name,
+#         "custtel": phone,
+#         "delivery": time,
+#         "size": size,
+#         "topping": toppings
+#     }
+# }
+#
+# URL = "httpbin.org/post"
+#
+# response = requests.post(URL, json=data)
+# print(response.json())
 
-data = {
-    "form": {
-        "comments": comments,
-        "custemail": email,
-        "custname": name,
-        "custtel": phone,
-        "delivery": time,
-        "size": size,
-        "topping": toppings
-    }
-}
+#========================================================
 
-URL = "httpbin.org/post"
+# Zad5
+# NO SERVER
+# DO NOT DO?
 
-response = requests.post(URL, json=data)
-print(response.json())
+# =======================================================
+
+# Zad6
+# NO SERVER
+# DO NOT DO?
