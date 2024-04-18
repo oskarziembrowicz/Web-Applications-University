fc73a78930faf44f4a2565d9dd4dbbef877d6597
oskarziembrowicz <rakson2002@gmail.com>
2024-04-18 13:29:54 +0200

    Adding for loop to ex 2 server

--- lb7/lab7zad2server.py
+++ lb7/lab7zad2server.py
@@ -21,33 +21,38 @@ print(f"Generated number: {random_number}")
 while True:
     read_socket, write_socket, error_socket = select.select(connected_clients_sockets, [], [])
 
-    if read_socket == server_socket:
-        client_socket, client_address = server_socket.accept()
-        print("Client connected")
-    else:
-        try:
-            data = read_socket.recv(1024)
-            if data:
-                print(f"Recieved a message: {data.decode()}")
-                try:
-                    number = int(data.decode())
-                    message = ''
-                    if number == random_number:
-                        message = "Congratulations! You guessed it!"
-                        random_number = randint(1, 100)
-                        print("Number was guessed")
-                        print(f"New generated number: {random_number}")
-                    elif number < random_number:
-                        message = "Wrong! Too low"
-                    elif number > random_number:
-                        message = "Wrong! Too high"
-                    print(f"Sending message: {message}")
-                    read_socket.send(message.encode())
-                except Exception as e:
-                    message = "Incorrect type " + e
-                    read_socket.send(message.encode())
-        except:
-            read_socket.close()
-            print("Client disconnected")
+    for client in read_socket:
+
+        if client == server_socket:
+            client_socket, client_address = server_socket.accept()
+            connected_clients_sockets.append(client_socket)
+            print("Client connected")
+        else:
+            try:
+                data = read_socket.recv(1024)
+                if data:
+                    print(f"Recieved a message: {data.decode()}")
+                    try:
+                        number = int(data.decode())
+                        message = ''
+                        if number == random_number:
+                            message = "Congratulations! You guessed it!"
+                            random_number = randint(1, 100)
+                            print("Number was guessed")
+                            print(f"New generated number: {random_number}")
+                        elif number < random_number:
+                            message = "Wrong! Too low"
+                        elif number > random_number:
+                            message = "Wrong! Too high"
+                        print(f"Sending message: {message}")
+                        client.send(message.encode())
+                    except Exception as e:
+                        message = "Incorrect type " + e
+                        client.send(message.encode())
+            except:
+                client.close()
+                connected_clients_sockets.remove(client)
+                print("Client disconnected")
+                continue
 
 server_socket.close()
