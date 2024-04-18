9d4cc93bd4873a8989376ab3e16fef8a2d6bd0fe
oskarziembrowicz <rakson2002@gmail.com>
2024-04-18 08:43:42 +0200

    finishing lb6 and starting lb7

--- lb6/lab6client.py
+++ lb6/lab6client.py
@@ -1,18 +1,69 @@
-import socket
+import smtplib, ssl
+import base64
 
-# Zad 6
+# Zad6
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.settimeout(5)
+# smtp_server = 'smtp.gmail.com'
+# port = 587
+# sender = input("Enter sender email address: ")
+# password = input("Enter your password: ")
+#
+# receiver = input("Enter receiver's email address: ")
+# message = input("Enter your message: ")
+#
+# context = ssl.create_default_context()
+#
+# with smtplib.SMTP(smtp_server, port) as server:
+#     server.ehlo()
+#     server.starttls(context=context)
+#     server.ehlo()
+#     server.login(sender, password)
+#     print("Login successful")
+#     server.sendmail(sender, receiver, message)
 
-print(socket.gethostbyname("interia.pl"))
+#===============================================================
 
-try:
-    client_socket.connect((socket.gethostbyname("interia.pl"), 587))
-    client_socket.send("EHLO testowanieaplikacji.edu.pl".encode())
-    response = client_socket.recv(1024)
-    print(response.decode())
-except socket.error as e:
-    print(f"Socket error: {e}")
+# Zad 7
 
-client_socket.close()
+smtp_server = 'smtp.gmail.com'
+port = 587
+sender = input("Enter sender email address: ")
+password = input("Enter your password: ")
+
+receiver = input("Enter receiver's email address: ")
+subject = input("Enter subject: ")
+message = input("Enter your message: ")
+
+f = open("text.txt", "r")
+textfile = f.read()
+
+text_bytes = textfile.encode('ascii')
+base64_text = base64.b64encode(text_bytes)
+
+data = f"""\
+From: {sender}
+To: {receiver}
+Subject: {subject}
+MIME Version: 1.0
+Content-Type: multipart/mixed; boundary=\"sep\"
+--sep
+{message}
+--sep
+Content-Type: plain/text; name=\"text.txt\"
+Content-Disposition: attachment; filename=\"text.txt\"
+Content-Transfer-Encoding: base64
+{base64_text}
+--sep--"""
+
+print(data)
+
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP(smtp_server, port) as server:
+    server.ehlo()
+    server.starttls(context=context)
+    server.ehlo()
+    server.login(sender, password)
+    print("Login successful")
+    server.sendmail(sender, receiver, message)
