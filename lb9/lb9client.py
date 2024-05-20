13349d469aaf6fbb7ae96165cfa458bb3f9b6cf8
oskarziembrowicz <rakson2002@gmail.com>
2024-05-20 12:59:45 +0200

    Ex 2 updated, finished ex 3

--- lb9/lb9client.py
+++ lb9/lb9client.py
@@ -10,8 +10,27 @@ import websocket
 
 # Zad2
 
-ws = websocket.WebSocket()
-ws.connect('wss://echo.websocket.org/')
-print(ws.recv())
-ws.send_text("Hello!")
-print(ws.recv())
+# ws = websocket.WebSocket()
+# url = 'wss://echo.websocket.org/'
+# ws.connect(url, max_size=125)
+# print(ws.recv())
+# message = input("Enter your message: ")
+# if len(message.encode('utf-8')) <= 125:
+#     ws.send(message)
+#     print(ws.recv())
+# else:
+#     print("Message exceeds 125 bytes")
+
+#==============================================
+
+# Zad3
+
+# ws = websocket.WebSocket()
+# url = 'wss://echo.websocket.org/'
+# ws.connect(url)
+# print(ws.recv())
+# message = input("Enter your message: ")
+# ws.send_text(message)
+# print(ws.recv())
+
+#=============================================
