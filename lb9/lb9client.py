8d9c74fdbf45b9c2e82adc80c95714d7f9fa74a6
oskarziembrowicz <rakson2002@gmail.com>
2024-05-20 12:21:16 +0200

    Ex 2 complete

--- lb9/lb9client.py
+++ lb9/lb9client.py
@@ -2,8 +2,16 @@ import websocket
 
 # Zad1
 
+# ws = websocket.WebSocket()
+# ws.connect('wss://echo.websocket.org/')
+# print(ws.recv())
+
+#==============================================
+
+# Zad2
+
 ws = websocket.WebSocket()
 ws.connect('wss://echo.websocket.org/')
 print(ws.recv())
-
-#==============================================
+ws.send_text("Hello!")
+print(ws.recv())
