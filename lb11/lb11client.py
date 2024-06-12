c974ae95bcbada3cbce5c49f3f6019273db82e1c
oskarziembrowicz <rakson2002@gmail.com>
2024-06-12 19:32:18 +0200

    Failed to connect to chat in ex3

--- lb11/lb11client.py
+++ lb11/lb11client.py
@@ -38,27 +38,45 @@ import json
 
 # chat.freenode.net
 
-API_KEY = '9df98a19d232f30530ad7b157249eff7'
 
-def getWeather(city):
-    api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
-
-    response = requests.get(api_url)
-    weather = json.loads(response.text)
-    # print(weather)
-    weather_type = weather["weather"][0]["description"]
-    temperature = weather['main']['temp']
-    pressure = weather['main']['pressure']
-    humidity = weather['main']['humidity']
-    wind = weather['wind']['speed']
-
-    message = f'Weather: {weather_type}\nTemperature: {temperature} degrees\nPressure: {pressure}\nHumidity: {humidity}%\nWind Speed: {wind}m/h'
-
-    return message
-
-
-city = "Lublin"
-print(getWeather(city))
+# API_KEY = '9df98a19d232f30530ad7b157249eff7'
+#
+# def getWeather(city):
+#     api_url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}'
+#
+#     response = requests.get(api_url)
+#     weather = json.loads(response.text)
+#     # print(weather)
+#     weather_type = weather["weather"][0]["description"]
+#     temperature = weather['main']['temp']
+#     pressure = weather['main']['pressure']
+#     humidity = weather['main']['humidity']
+#     wind = weather['wind']['speed']
+#
+#     return f'Weather: {weather_type}\nTemperature: {temperature} degrees\nPressure: {pressure}\nHumidity: {humidity}%\nWind Speed: {wind}m/h'
+#
+# HOST = 'chat.freenode.net'
+# PORT = 7000
+#
+# connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# # client_socket = ssl.wrap_socket(client_socket, server_side=False, keyfile='lb11ex5server.key', certfile='lb11ex5server.crt')
+#
+# try:
+#     connection.connect((HOST, PORT))
+#     connection.send("{} {}\r\n".format('NICK', 'chatbot1').encode())
+#     connection.send("{} {}\r\n".format('JOIN', '#chat1').encode())
+#     data = connection.recv(4096)
+#     if data:
+#         print(f"Recieved: {data.decode()}")
+#     else:
+#         print("No data recieved :(")
+# except socket.error as e:
+#     print(f"Socket error: {e}")
+#
+# connection.close()
+#
+# # city = "Lublin"
+# # print(getWeather(city))
 #============================================
 
 # Zad5
